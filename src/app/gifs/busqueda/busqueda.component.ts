a790bf60088b45e000cc4fd7b7741e440010fa6e
cabrerojas <gcabrerojas@gmail.com>
2021-01-26 16:39:40 -0300

    update buscador

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent  {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar( termino:string ) {
+
+    const valor = this.txtBuscar.nativeElement.value;
+
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
+
+  }
+
+}
