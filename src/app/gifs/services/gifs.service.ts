e54fb7f02129dded0df5dfe0bf7ae8b4021c1e57
cabrerojas <gcabrerojas@gmail.com>
2021-01-27 22:58:31 -0300

    localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -19,7 +19,12 @@ export class GifsService {
     return [...this._historial ];
   }
 
-  constructor( private http: HttpClient){}
+  constructor( private http: HttpClient){
+   
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+    
+  }
 
   buscarGifs( query:string = ''){
 
@@ -28,6 +33,9 @@ export class GifsService {
     if ( !this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify( this._historial ));
+
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=57d4NEX8O2IOigO3MTDDiVurxVcDC971&q=
@@ -35,6 +43,8 @@ export class GifsService {
       .subscribe( ( resp ) => {
         console.log(resp.data);
         this.resultados = resp.data;
+
+        localStorage.setItem('resultados', JSON.stringify( this.resultados ));
         
       });
 
